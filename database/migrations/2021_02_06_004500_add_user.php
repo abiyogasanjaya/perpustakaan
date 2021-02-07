923f0fd711f67d7bc9e445c9a4604fc722fb7f21
Project Learn Abiyoga <projectlearnabiyoga@gmail.com>
2021-02-07 14:15:47 +0800

    Atur Posisi

--- database/migrations/2021_02_06_004500_add_user.php
+++ database/migrations/2021_02_06_004500_add_user.php
@@ -23,6 +23,15 @@ class AddUser extends Migration
                 'level_user' => 1,
             )
         );
+        DB::table('users')->insert(
+            array(
+                'name' => 'Kharis',
+                'email' => 'kharis@gmail.com',
+                'password' => md5('admin_123_!'),
+                'remember_token' => md5(date('Y-m-d H:i:s').rand(1000,9999)),
+                'level_user' => 1,
+            )
+        );
     }
 
     /**
